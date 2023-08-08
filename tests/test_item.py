db9f3f0fa159d3364035c7b4dbe41fb3306b7fc3
Dzerbun Igor <dzerbuni@mail.ru>
2023-08-08 22:45:57 +0300

    'Homework1'

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("Книга", 10.0, 5)
+    assert item.calculate_total_price() == 50.0
+
+
+def test_apply_discount():
+    item = Item("Книга", 10.0, 5)
+    item.pay_rate = 0.9  # Устанавливаем скидку в 10%
+    item.apply_discount()
+    assert item.price == 9.0
