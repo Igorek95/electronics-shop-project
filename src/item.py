e1e5cade2d8e719378a7d5049436cd8537827446
Dzerbun Igor <dzerbuni@mail.ru>
2023-08-09 23:11:55 +0300

    Homework2

--- src/item.py
+++ src/item.py
@@ -1,22 +1,49 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
+
     pay_rate = 1.0
+    """Коэффициент для расчета цены с учетом скидки."""
+
     all = []
+    """Список всех созданных экземпляров товаров."""
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        Создание экземпляра класса item.
+        Создание экземпляра класса Item.
 
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)  # Добавляем текущий экземпляр в список all
+        self.all.append(self)
+
+    @property
+    def name(self):
+        """Возвращает наименование товара."""
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        """
+        Устанавливает наименование товара.
+
+        Если длина наименования превышает 10 символов, обрезает его до 10 символов.
+
+        :param value: Новое наименование товара.
+        """
+        if len(value) > 10:
+            self.__name = value[:10]
+        else:
+            self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +59,32 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Создает экземпляры класса Item из данных CSV-файла.
+
+        :return: Список экземпляров товаров.
+        """
+        items = []
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_path = os.path.join(current_dir, '..', 'src', 'items.csv')
+        with open(csv_path, 'r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+        return items
+
+    @staticmethod
+    def string_to_number(value):
+        """
+        Преобразует строку в число.
+
+        :param value: Строка, представляющая число.
+        :return: Преобразованное число.
+        """
+        return float(value)
--- tests/test_item.py
+++ tests/test_item.py
@@ -11,3 +11,35 @@ def test_apply_discount():
     item.pay_rate = 0.9  # Устанавливаем скидку в 10%
     item.apply_discount()
     assert item.price == 9.0
+
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_name_setter_too_long():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'СуперСмартфон'
+    assert item.name == 'СуперСмарт'
+
+
+def test_instantiate_from_csv():
+    items = Item.instantiate_from_csv()
+    assert len(items) == 5
+
+
+def test_item_creation_from_csv():
+    items = Item.instantiate_from_csv()
+    assert items[0].name == 'Смартфон'
+    assert items[1].name == 'Ноутбук'
+    assert items[2].name == 'Кабель'
+    assert items[3].name == 'Мышка'
+    assert items[4].name == 'Клавиатура'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5.5
